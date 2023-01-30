f205bfd1917877072ab2d1f80be7a54284bc4e82
RoryT-U <rorytu@gmail.com>
2023-01-30 14:29:21 +1100

    Small formatting changes

--- src/views/View.tsx
+++ src/views/View.tsx
@@ -1,11 +1,12 @@
 import { D2Renderer } from "../renderer/D2Renderer/D2Renderer";
 import trace from "../data/grid-astar.trace.json"
+import trace2 from "../data/tile.trace.json"
 import {parseViews} from "../renderer/Parser";
 
 const parsedComps = parseViews(trace.render).main
 
 export function View() {
-
+  console.log(parsedComps)
   return (
     <>
       <D2Renderer parsedComps={parsedComps} />
